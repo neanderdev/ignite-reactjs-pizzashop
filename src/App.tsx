c67490958d151cbd9c59e492a4af4526610a06ed
Neander de Souza <neander.faria@hotmail.com>
2024-04-11 15:42:51 -0300

    Setup do projeto - Configurando ESLint e Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './global.css'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex h-screen items-center justify-center'>
       <Button>Enviar</Button>
     </div>
   )
