554207e845404692d008371acfdcf3fa4bf50945
Neander de Souza <neander.faria@hotmail.com>
2024-04-12 10:57:53 -0300

    Conexão com API - Listagem de pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
+
+import { getOrders } from '@/api/get-orders'
 
 import { Pagination } from '@/components/pagination'
 import {
@@ -13,6 +16,11 @@ import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
   return (
     <>
       <Helmet title='Pedidos' />
@@ -47,9 +55,10 @@ export function Orders() {
             </TableHeader>
 
             <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
+              {result &&
+                result.orders.map((order) => {
+                  return <OrderTableRow key={order.orderId} order={order} />
+                })}
             </TableBody>
           </Table>
         </div>
