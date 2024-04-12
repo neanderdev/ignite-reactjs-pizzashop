7b23aaccdcee18660913b41401401674537affec
Neander de Souza <neander.faria@hotmail.com>
2024-04-12 10:06:30 -0300

    Conexão com API - Autenticação do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+
+import { queryClient } from './lib/react-query'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 
@@ -16,7 +19,9 @@ export function App() {
 
         <Toaster richColors />
 
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
