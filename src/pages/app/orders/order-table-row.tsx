9bca9cbaa60289b4e618ca086b551855145b3314
Neander de Souza <neander.faria@hotmail.com>
2024-04-12 09:33:10 -0300

    Páginas e componentes (UI) - Componente: Detalhes do pedido

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,17 +1,26 @@
 import { ArrowRight, Search, X } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { OrderDetails } from './order-details'
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant='outline' size='xs'>
-          <Search className='h-3 w-3' />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant='outline' size='xs'>
+              <Search className='h-3 w-3' />
 
-          <span className='sr-only'>Detalhes do pedido</span>
-        </Button>
+              <span className='sr-only'>Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
 
       <TableCell className='font-mono text-xs font-medium'>
@@ -34,7 +43,7 @@ export function OrderTableRow() {
 
       <TableCell>
         <Button variant='outline' size='xs'>
-          <ArrowRight className='mr-2 h-3 w-3' /> 
+          <ArrowRight className='mr-2 h-3 w-3' />
 
           Aprovar
         </Button>
@@ -42,8 +51,8 @@ export function OrderTableRow() {
 
       <TableCell>
         <Button variant='ghost' size='xs'>
-          <X className='mr-2 h-3 w-3' /> 
-          
+          <X className='mr-2 h-3 w-3' />
+
           Cancelar
         </Button>
       </TableCell>
